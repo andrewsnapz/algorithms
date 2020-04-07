30ff38f43319bac5cb458c3bccd1c8dbba6a4f92
Andrew Shin <andyrpg@gmail.com>
2020-04-07 15:59:47 -0700

    updated an algo

--- moveZeros.js
+++ moveZeros.js
@@ -1,22 +1,31 @@
-function moveZeros(nums) {
-    let indexes = [];
-    let count = 0;
-    for (let i = 0; i < nums.length; i++) { 
-        if (nums[i] === 0) {
-            indexes.push(i);
-        }
-    }
-    for (let i = 0; i < indexes.length; i++) { 
-        nums.splice(indexes[i], 1);
-        count = indexes.length;
-        if (count !== 0) { 
-            nums.push(0);
-        }
-    }
-    return nums;
-}
+// function moveZeroes(nums) {
+//     let indexes = [];
+//     let count = 0;
+//     for (let i = 0; i < nums.length; i++) { 
+//         if (nums[i] === 0) {
+//             indexes.push(i);
+//         }
+//     }
+//     for (let i = 0; i < indexes.length; i++) { 
+//         nums.splice(indexes[i], 1);
+//         count = indexes.length;
+//         if (count !== 0) { 
+//             nums.push(0);
+//         }
+//     }
+//     return nums;
+// }
 
+var moveZeroes = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0 && nums[i + 1] !== 0) {
+            nums.push(nums.shift());
+            i = i - 1;
+        }
+    }
+    return nums.reverse();
+};
 
 let example1 = [1,0,0,1]; 
-console.log(moveZeros(example1)); //[1,1,0,0];
-console.log(moveZeros([1,0,1])); //[1,1,0];
+console.log(moveZeroes(example1)); //[1,1,0,0];
+console.log(moveZeroes([1,0,1])); //[1,1,0];
