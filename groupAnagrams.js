fa830333f25ad356475f25ed423267b3703bac1d
Andrew Shin <andyrpg@gmail.com>
2020-04-09 16:19:06 -0700

    solved groupAnagrams

--- groupAnagrams.js
+++ groupAnagrams.js
@@ -1,15 +1,15 @@
-function groupAnagrams(strs, list = []) {
-    let cache = {};
-    let pointer = 0;
-    for (let i = 0; i < strs[0].length; i++) {
-        if (!cache[strs[0][i]]) {
-            cache[strs[0][i]] = 1;
-        } else {
-            cache[strs[0][i]]++;
-        }
-    }
+// function groupAnagrams(strs, list = []) {
+//     let cache = {};
+//     let pointer = 0;
+//     for (let i = 0; i < strs[0].length; i++) {
+//         if (!cache[strs[0][i]]) {
+//             cache[strs[0][i]] = 1;
+//         } else {
+//             cache[strs[0][i]]++;
+//         }
+//     }
     
-}
+// }
 
 let input = ["eat", "tea", "tan", "ate", "nat", "bat"];
 console.log(groupAnagrams(input)); 
@@ -20,3 +20,19 @@ console.log(groupAnagrams(input));
   ["bat"]
 ]
 */ 
+
+function groupAnagrams(strs) {
+    let anagramArr = [];
+    let anagramCache = {};
+    for (let i = 0; i < strs.length; i++) {
+        let sortedWord = strs[i].split('').sort();
+        if (!anagramCache[sortedWord]) {
+            anagramCache[sortedWord] = [];
+            anagramCache[sortedWord].push(strs[i]);
+        } else {
+            anagramCache[sortedWord].push(strs[i]);
+        }
+    }
+    return Object.values(anagramCache);
+}
+
