8177a56d4adbf303c6ca29f7ba41b613261a0f1d
Andrew Shin <andyrpg@gmail.com>
2020-08-20 13:31:24 -0700

    fixed zigzag transversal

--- zigzagLevelOrder.js
+++ zigzagLevelOrder.js
@@ -44,39 +44,62 @@
 //     console.log(arr)
 // };
 
-var zigzagLevelOrder = function(root) {
-    if (!root) return [];
-    let obj = {};
-    let output = [];
+// var zigzagLevelOrder = function(root) {
+//     if (!root) return [];
+//     let obj = {};
+//     let output = [];
 
-    const dfs = (queue, depth = 1) => {
-        while(queue.length) {
-            let node = queue[0];
-            if (!obj[depth]) obj[depth] = [];
-            obj[depth].push(node.val);
+//     const dfs = (queue, depth = 1) => {
+//         while(queue.length) {
+//             let node = queue[0];
+//             if (!obj[depth]) obj[depth] = [];
+//             obj[depth].push(node.val);
             
-            if (node.left) {
-                queue.push(node.left, depth + 1);
-            }
+//             if (node.left) {
+//                 queue.push(node.left, depth + 1);
+//             }
             
-            if (node.right) {
-                queue.push(node.right, depth + 1);
-            }
+//             if (node.right) {
+//                 queue.push(node.right, depth + 1);
+//             }
 
-            if (node.left && node.right) {
-                depth++;
-            }
+//             if (node.left && node.right) {
+//                 depth++;
+//             }
             
-            queue.shift();
+//             queue.shift();
+//         }
+//     }
+//     dfs([root]);
+
+//     for (let key in obj) {
+//         if (parseInt(key) % 2 === 0) {
+//             obj[key] = obj[key].reverse();
+//         }
+//     };
+
+//     return output.push(Object.values(obj));
+// };
+
+function zigzagLevelOrder(root) {
+    let result = {};
+
+    function transverse(node, depth) {
+        if (!node) return;
+        if (!result[depth]) result[depth] = [node.val];
+        else result[depth].push(node.val);
+
+        transverse(node.left, depth + 1);
+        transverse(node.right, depth + 1);
+    }
+
+    transverse(root, depth);
+
+    for (let key in result) {
+        if (parseInt(key) % 2 === 0) {
+            result[depth].reverse();
         }
     }
-    dfs([root]);
 
-    for (let key in obj) {
-        if (parseInt(key) % 2 === 0) {
-            obj[key] = obj[key].reverse();
-        }
-    };
-
-    return output.push(Object.values(obj));
-};
+    return Object.values(result);
+}
