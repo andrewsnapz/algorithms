9fe70316485f1173bbcf3880e162dd2dfd85f211
Andrew Shin <andyrpg@gmail.com>
2020-09-21 14:56:12 -0700

    added building heaps

---
+++ buildingHeap.js
@@ -0,0 +1,24 @@
+class binaryTree {
+    constructor(val) {
+        this.value = val;
+        this.left = null;
+        this.right = null;
+    }
+
+    addingNode(val) {
+        let node = new binaryTree(val);
+        if (!this.left && !this.right) {
+            
+        } 
+    }
+}
+
+
+function buildingMaxHeap(arr) {
+
+};
+
+
+
+console.log(buildingMinHeap([1,3,5,4,6,13,10,9,8,15,17]));
+
