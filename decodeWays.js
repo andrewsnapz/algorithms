54d4e0313b2722afdd6dab4e0c04d2f3c817d879
Andrew Shin <andyrpg@gmail.com>
2021-07-08 17:26:02 -0700

    bubbleSort

--- decodeWays.js
+++ decodeWays.js
@@ -1,11 +1,11 @@
-const numDecodings = (s, count = 0) => {
-  let sArr = s.split("").map((x) => parseInt(x, 10));
-  console.log(sArr);
-  let pt1 = 0;
-  let pt2 = pt1 + 1;
-  while(pt1 < s.length) {
-      if (s[pt1])
-  }
+const numDecodings = (s, i = 0, memo = {}) => {
+  if (i in memo) return memo[i];
+  if (s[i] === "0") return 0;
+  if (i >= s.length - 1) return 1;
+  memo[i] =
+    numDecodings(s, i + 1, memo) +
+    (s[i] + s[i + 1] < 27 ? numDecodings(s, i + 2, memo) : 0);
+  return memo[i];
 };
 
 // questions to ask:
@@ -14,5 +14,7 @@ const numDecodings = (s, count = 0) => {
 
 // is the numberStr even or odd?
 //ASCII: A(65) - Z(90)
+// adding functions?
 
 console.log(numDecodings("12")); // 2
+console.log(numDecodings("123")); // 3
