937f622e2621fca6b867230e8f0475de25402bd6
Andrew Shin <andyrpg@gmail.com>
2021-07-29 16:22:39 -0700

    edited removeDuplicates from CTCI

--- CTCI/LinkedLists/removeDuplicates.js
+++ CTCI/LinkedLists/removeDuplicates.js
@@ -17,6 +17,35 @@ const LinkedList = require("./jsSinglyLinkedList");
     }
 */
 
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+
+  addNode(val) {
+    let n = this;
+    let end = new Node(val);
+
+    while (n.next !== null) {
+      n = n.next;
+    }
+    n.next = end;
+    return n;
+  }
+
+  getValues() {
+    let n = this;
+    let values = [];
+
+    while (n.next !== null) {
+      values.push(n.val);
+      n = n.next;
+    }
+    return values;
+  }
+}
+
 /*
 Remove Duplicates: write code that removes duplicates from
 an unsorted linked list
@@ -25,46 +54,43 @@ FOLLOW UP:
 How would you solve this problem if a temporary buffer is not allowed?
 */
 
-// const removeDuplicates = (list) => {
-//   let pt1 = list.head;
-//   let pt2 = list.head.next;
+// const removeDuplicates = (head) => {
+//   let pt = head;
+//   let set = new Set();
 
-//   while (pt2.next !== null) {
-//     if (pt2.next === null) {
-//       pt1 = pt1.next;
-//       pt2 = pt1.next;
-//     }
-
-//     if (pt1.val !== pt2.val) {
-//       pt2 = pt2.next;
-//     } else {
-//       let adjustedValue = pt2.next;
-//       let curr = pt2;
-
-//       pt2.next = adjustedValue;
-//       pt2.val = adjustedValue.val;
-
-//       pt2 = pt2.next;
-//     }
+//   while (pt) {
+//     set.add(pt);
+//     pt = pt.next;
 //   }
-
-//   console.log(iteriateLinkList(list));
-//   return list;
+//   console.log(set);
 // };
 
 const removeDuplicates = (head) => {
-  let pt = head;
-  let set = new Set();
+  if (!head) return head;
+  let target = head;
+  let pt1 = head;
+  let pt2 = head.next;
 
-  while (pt) {
-    set.add(pt);
-    pt = pt.next;
+  while (target) {
+    while (pt2.next !== null) {
+      if (pt2.val === target.val) {
+        pt1.next = pt2.next;
+      }
+      pt1 = pt1.next;
+      pt2 = pt2.next;
+    }
+
+    if (!target.next) break;
+
+    target = target.next;
+    pt1 = target;
+    pt2 = pt1.next;
   }
-  console.log(set);
+
+  return head;
 };
 
-let linkList = new LinkedList();
-linkList.addNode(1);
+let linkList = new Node(1);
 linkList.addNode(1);
 linkList.addNode(2);
 linkList.addNode(3);
@@ -72,49 +98,4 @@ linkList.addNode(1);
 linkList.addNode(3);
 
 console.log(removeDuplicates(linkList));
-
-// class Node {
-//   constructor(data = null) {
-//     this.data = data;
-//     this.next = null;
-//   }
-// }
-
-// const removeDuplicates = (headNode) => {
-//   let cur = headNode;
-//   let visited = new Set([cur.data]);
-//   while (cur.next) {
-//     let nextnode = cur.next;
-//     if (visited.has(nextnode.data)) {
-//       // if current node data is visited, skip
-//       cur.next = nextnode.next;
-//     } else {
-//       // if current node data is not visited, visit
-//       visited.add(nextnode.data);
-//       cur = nextnode;
-//     }
-//   }
-//   return headNode;
-// };
-
-// // prepare data
-// const arr = [1, 2, 1, 3, 4, 2, 1];
-// let head, prevNode;
-// while (arr.length > 0) {
-//   let node = new Node(arr.pop());
-//   if (prevNode) {
-//     node.next = prevNode;
-//   }
-//   head = node;
-//   prevNode = node;
-// }
-
-// // remove duplicate
-// const headAfterRemove = removeDuplicates(head);
-
-// // verify result
-// let cursor = headAfterRemove;
-// while (cursor) {
-//   console.log(cursor.data);
-//   cursor = cursor.next;
-// }
+console.log(linkList.getValues());
